8fcef63ba7da80016f04bda7540de1e6e49615f2
Raj Shukla <rajdragatron918@gmail.com>
2022-10-13 01:33:37 -0400

    Trip Action push

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+// import 'package:ninja_trips/screens/home.dart';
+import 'package:ninja_trips/screens/sandbox.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Ninja Trips',
+      // home: Home(),
+      home: Sandbox()
+    );
+  }
+}
